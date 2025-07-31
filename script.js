8833dd389a699eb4406d5b8ca343db87793f5ff6
Xolani-Dev99 <xolanindunge@gmail.com>
2025-07-31 15:01:57 +0200

    Initial commit

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-// script.js
-window.onload = () => {
-    console.log("Creche website loaded successfully.");
-};
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("Creche website loaded successfully.");
+
+  const toggleButton = document.querySelector('.toggle-button');
+  const navbarLinks = document.querySelector('.navbar-links');
+
+  if (toggleButton && navbarLinks) {
+    toggleButton.addEventListener('click', () => {
+      navbarLinks.classList.toggle('active');
+    });
+  }
+});
